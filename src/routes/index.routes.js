f150b6c5609c7333ce578c4db50b112dd7489f87
astradae <astradaezequiel.tpi@gmail.com>
2022-08-26 17:55:27 -0300

    add main.routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const mainRouter = require('./main.routes')
 const cardRouter = require('./card.routes')
 const imageRouter = require('./image.routes')
 const adminRouter = require('./admin.routes')
 
+router.use('/', mainRouter)
 router.use('/api/cards', cardRouter)
 router.use('/api/images', imageRouter)
 router.use('/api/admin', adminRouter)
