c5973fa7178ecaa9e07d607123cc994c2e97a43f
astradae <astradaezequiel.tpi@gmail.com>
2022-08-30 09:24:17 -0300

    index route add payment

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,10 +5,12 @@ const mainRouter = require('./main.routes')
 const cardRouter = require('./card.routes')
 const imageRouter = require('./image.routes')
 const adminRouter = require('./admin.routes')
+const paymentRouter = require('./payment.routes')
 
 router.use('/', mainRouter)
 router.use('/api/cards', cardRouter)
 router.use('/api/images', imageRouter)
 router.use('/api/admin', adminRouter)
+router.use('/api/payment', paymentRouter)
 
 module.exports = router;
