135f258e5639eafc4cc6a180a3783d64914dd761
astradae <astradaezequiel.tpi@gmail.com>
2022-09-01 23:08:13 -0300

    paypal add controllers

---
+++ src/controllers/paypal.controllers.js
@@ -0,0 +1,35 @@
+const paypalService = require("../services/paypal.services");
+
+const paypal = require('@paypal/checkout-server-sdk')
+const {client} = require ('../lib/paypal_client')
+
+const pay = async (req, res, next) => {
+  const value = req.body.value
+  try {
+    const request = new paypal.orders.OrdersCreateRequest()
+
+    request.headers["Prefer"] = "return=Representation"
+
+    request.requestBody({
+        intent:"CAPTURE",
+        purchase_units:[
+            {
+                amount:{
+                    value:value,
+                    currency_code:"USD"
+                }
+            }
+        ]
+    })
+
+    const response = await client.execute(request)
+
+    return res.status(200).json({orderID:response.result.id})
+  } catch (e) {
+    next(e)
+  }
+};
+
+module.exports = {
+    pay
+}
