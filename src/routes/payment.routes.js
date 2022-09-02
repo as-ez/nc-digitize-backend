009e107fb658e1b488bf0f211b7b0772efe0337e
astradae <astradaezequiel.tpi@gmail.com>
2022-09-01 23:08:59 -0300

    add paypal route

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const stripeController = require("../controllers/stripe.controllers");
+const paypalController = require("../controllers/paypal.controllers");
 
 router.post('/stripe', stripeController.pay)
+router.post('/paypal', paypalController.pay)
 
 module.exports = router;
