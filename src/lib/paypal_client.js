db53f77234d3f45598d1819e18ba4af67e4e1699
astradae <astradaezequiel.tpi@gmail.com>
2022-09-01 23:20:50 -0300

    add env

--- src/lib/paypal_client.js
+++ src/lib/paypal_client.js
@@ -1,7 +1,8 @@
 const paypal = require('@paypal/checkout-server-sdk')
 const config = require("../config/index");
 
-const enviroment = new paypal.core.SandboxEnvironment(config.paypal.clientID, config.paypal.clientSecret);
+const env = process.env.NODE_ENV
+const enviroment =  env === "development" ? new paypal.core.SandboxEnvironment(config.paypal.clientID, config.paypal.clientSecret):new paypal.core.LiveEnvironment(config.paypal.clientID, config.paypal.clientSecret);
 
 const client = new paypal.core.PayPalHttpClient(enviroment)
 
