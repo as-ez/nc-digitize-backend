4f1894aca8150cf6da17756e367eb775b3602de1
astradae <astradaezequiel.tpi@gmail.com>
2022-09-29 21:42:29 -0300

    delete dist

--- dist/routes/payment.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const stripeController = require("../controllers/stripe.controllers");
-
-router.post('/stripe', stripeController.createCheckoutSession);
-module.exports = router;
