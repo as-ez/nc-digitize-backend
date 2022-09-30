c7f2ade4fe0739f18945bfefada0742e9697762f
astradae <astradaezequiel.tpi@gmail.com>
2022-09-29 21:37:59 -0300

    add jwt

---
+++ src/module/jwt/auth.js
@@ -0,0 +1,19 @@
+const { sign, verify }= require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const generateToken = (id) => {
+  const jwt = sign({ id }, JWT_SECRET, {
+    expiresIn: "2h",
+  });
+  return jwt;
+};
+
+const verifyToken = (jwt) => {
+  const isOk = verify(jwt, JWT_SECRET);
+  return isOk;
+};
+
+module.exports = { 
+    generateToken, 
+    verifyToken 
+};
